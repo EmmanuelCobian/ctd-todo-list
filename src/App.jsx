8cf3a969e3c50555b3b7ed78a0457faa2a957418
Emmanuel <ecobian12310@gmail.com>
2026-01-08 16:13:36 -0800

    fetching from airtable and displaying todos and error messages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css';
 import TodoList from './features/TodoList/TodoList';
 import TodoForm from './features/TodoForm';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
   const [todoList, setTodoList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const url = `https://api.airtable.com/v0/${import.meta.env.VITE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}`;
+  const token = `Bearer ${import.meta.env.VITE_PAT}`;
 
   const completeTodo = (id) => {
     const updatedTodos = todoList.map((todo) => {
@@ -31,11 +35,62 @@ function App() {
     setTodoList(updatedTodos);
   };
 
+  useEffect(() => {
+    const fetchTodos = async () => {
+      setIsLoading(true);
+      const options = {
+        method: 'GET',
+        headers: {
+          Authorization: token,
+        },
+      };
+
+      try {
+        const resp = await fetch(url, options);
+        if (!resp.ok) {
+          throw new Error(resp.message);
+        }
+
+        const response = await resp.json();
+        const todos = response.records.map((record) => {
+          const todo = {
+            title: record.fields.title,
+            id: record.id,
+            isCompleted: false,
+          };
+
+          if (Object.hasOwn(record.fields, 'isCompleted') && record.fields.isCompleted) {
+            todo.isCompleted = true;
+          }
+
+          return todo;
+        });
+        setTodoList(todos);
+      } catch (error) {
+        setErrorMessage(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchTodos();
+  }, []);
+
+  if (errorMessage) {
+    return (
+      <div>
+        <hr />
+        <p>{errorMessage}</p>
+        <button onClick={() => setErrorMessage('')}>Clear error message</button>
+      </div>
+    );
+  }
+
   return (
     <div>
       <h1>My Todos</h1>
       <TodoForm onAddTodo={addTodo} />
-      <TodoList todoList={todoList} onCompleteTodo={completeTodo} onUpdateTodo={updateTodo} />
+      <TodoList todoList={todoList} onCompleteTodo={completeTodo} onUpdateTodo={updateTodo} isLoading={isLoading} />
     </div>
   );
 }
