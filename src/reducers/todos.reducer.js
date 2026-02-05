50f28378c4af137095dfdabdb65b505132472e9a
Emmanuel <ecobian12310@gmail.com>
2026-02-05 14:42:08 -0800

    filled out reducer functions

--- src/reducers/todos.reducer.js
+++ src/reducers/todos.reducer.js
@@ -26,46 +26,108 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case actions.fetchTodos:
+    case actions.fetchTodos: {
       return {
         ...state,
+        isLoading: true,
       };
-    case actions.loadTodos:
+    }
+
+    case actions.loadTodos: {
+      const todos = action.records.map((record) => {
+        const todo = {
+          title: record.fields.title,
+          id: record.id,
+          isCompleted: false,
+        };
+
+        if (Object.hasOwn(record.fields, 'isCompleted') && record.fields.isCompleted) {
+          todo.isCompleted = true;
+        }
+
+        return todo;
+      });
       return {
         ...state,
+        todoList: todos,
+        isLoading: false,
       };
-    case actions.setLoadError:
+    }
+
+    case actions.setLoadError: {
       return {
         ...state,
+        error: action.error.message,
+        isLoading: false,
       };
-    case actions.startRequest:
+    }
+
+    case actions.startRequest: {
       return {
         ...state,
+        isSaving: true,
       };
-    case actions.addTodo:
+    }
+
+    case actions.addTodo: {
+      const savedTodo = {
+        title: action.records[0].fields.title,
+        isCompleted: true,
+        id: action.records[0].id,
+      };
+      if (!action.records[0].fields.isCompleted) {
+        savedTodo.isCompleted = false;
+      }
+
       return {
         ...state,
+        todoList: [...state.todoList, savedTodo],
+        isSaving: false,
       };
-    case actions.endRequest:
+    }
+
+    case actions.endRequest: {
       return {
         ...state,
+        isLoading: false,
+        isSaving: false,
       };
-    case actions.updateTodo:
-      return {
-        ...state,
-      };
-    case actions.completeTodo:
-      return {
-        ...state,
-      };
+    }
+
     case actions.revertTodo:
+    case actions.updateTodo: {
+      const updatedTodos = state.todoList.map((todo) => {
+        if (todo.id === action.editedTodo.id) {
+          return action.editedTodo;
+        }
+        return todo;
+      });
+
+      const updatedState = {
+        ...state,
+        todoList: updatedTodos,
+        error: action.error ? action.error.message : '',
+      };
+      return updatedState;
+    }
+
+    case actions.completeTodo: {
+      const updatedTodos = state.todoList.map((todo) =>
+        todo.id === action.id ? { ...todo, isCompleted: true } : todo
+      );
       return {
         ...state,
+        todoList: updatedTodos,
       };
-    case actions.clearError:
+    }
+
+    case actions.clearError: {
       return {
         ...state,
+        error: '',
       };
+    }
+
     default:
       return state;
   }
